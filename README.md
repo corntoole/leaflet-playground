35c87c2b1bf2c532eb1f9801cd8f863416358eb2
Cornelius Toole <cornelius.toole@gmail.com>
2016-02-13 17:47:49 -0600

    added: logging of LatLng on click events
      - also removed access token from source
      - added function to load config
      - generated new access token

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ LeafletJS Playground
 --------------------
 
 ## A place to play around with [LeafletJS](http://leafletjs.com/).
+
+## To run this application
+
+Rename ```js/example.config.json``` to ```js/config.json```
+
+Add your access token (I used mapbox in this case) to ```js/config.json```
--- js/ex1.js
+++ js/ex1.js
@@ -1,14 +1,44 @@
-var map = L.map('map').setView([32.37475, -90.15294], 17);
+var map = L.map('map').setView([32.3601, -90.1544], 15);
 var lat = 32.37475;
 var lng = -90.15294;
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY29ybnRvb2xlIiwiYSI6ImNpa2w4dHdlZTBoODZ1Mm02aWZ1M3kwbjIifQ.dtwedSAYlUcU9tQrhxBf0A', {
-	maxZoom: 18,
-	id: 'mapbox.streets',
-	accessToken:  'pk.eyJ1IjoiY29ybnRvb2xlIiwiYSI6ImNpa2w4dHdlZTBoODZ1Mm02aWZ1M3kwbjIifQ.dtwedSAYlUcU9tQrhxBf0A'
-}).addTo(map);
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '/js/config.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
+
+var config = null;
+function init() {
+	loadJSON(function(resp){
+			config = JSON.parse(resp);
+			console.log(config);
+			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+config.mapboxAccessToken, {
+				maxZoom: 18,
+				id: 'mapbox.streets',
+				accessToken: config.mapboxAccessToken
+			}).addTo(map);
+	});
+}
+init();
 
 var latElm = document.querySelector('#lat');
 var lngElm = document.querySelector('#lng');
-console.log(latElm, lngElm);
-console.log('ex1.js loaded');
+
+function onMapClick(e) {
+    console.log("You clicked the map at " + e.latlng);
+		latElm.innerHTML = e.latlng.lat;
+		lngElm.innerHTML = e.latlng.lng;
+}
+map.on('click', onMapClick);
+
+
+var marker1 = L.marker([32.35868, -90.15761]).addTo(map);
+var marker2 = L.marker([32.3631, -90.1538]).addTo(map);
